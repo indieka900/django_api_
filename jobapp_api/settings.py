522f8059f46be61197206d316fd9cde6ac2c4a0c
indieka900 <indiekaj@gmail.com>
2023-06-12 14:18:19 +0300

    final

--- jobapp_api/settings.py
+++ jobapp_api/settings.py
@@ -15,7 +15,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-v3cwt_8%_dkzzm44_6h*-x^ej(ew*2rkgvyg2-3p4y$n_zslnk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
