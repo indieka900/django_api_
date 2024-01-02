fa12c77cb0955188e75f32b3525ca292fbc0b044
Joseph <indiekaj@gmail.com>
2024-01-02 14:33:30 +0300

    fixes-01

--- job_app/urls.py
+++ job_app/urls.py
@@ -6,10 +6,10 @@ from rest_framework_simplejwt.views import (
 )
 
 urlpatterns = [
+    path('', views.Job_list, name='Jobs'),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('register', views.UserRegistrationView.as_view(), name='users'),
-    path('jobs', views.Job_list, name='Jobs'),
     path('jobs/<str:pk>', views.get_job),
     path('job', views.searchJob),
     path('users/<str:pk>', views.UserDetail.as_view(), name='user-detail'),
